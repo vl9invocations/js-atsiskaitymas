3c9e7630426e4cd399a05fce42ec881dcb47ca9f
Administrator <victor@fluux.eu>
2025-01-09 18:48:59 +0200

    [0.0.1] First task

--- Task 1/script.js
+++ Task 1/script.js
@@ -10,3 +10,44 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const output = document.getElementById('output');
+const input = document.getElementById('search');
+const submitBtn = document.getElementById('submit-btn');
+
+submitBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    output.innerHTML = '';
+    if (input.value === '') {
+        alert('Please enter a value');
+        return;
+    }
+
+    if (isNaN(input.value)) {
+        alert('Please enter a number');
+        return;
+    }
+
+    output.innerHTML += `
+    <div style="${styles.output}">
+            <h2>${input.value} kg converted into:</h2>
+            <ol>
+                <li>Pounds (lb): ${Math.round(input.value * 2.2046 * 1000) / 1000}</li>
+                <li>Grams (g): ${input.value / 0.0010000}</li>
+                <li>Ounces (uz): ${Math.round(input.value * 35.274 * 1000) / 1000}</li >
+            </ol >
+    </div >
+    `;
+    return;
+});
+
+const styles = {
+    output: `
+        display: block;
+        margin: 0 auto;
+        max-width: 470px;
+        padding: 2em 4em;
+        background-color: #f7f7f7;;
+        line-height: 1.5;
+    `
+}   
